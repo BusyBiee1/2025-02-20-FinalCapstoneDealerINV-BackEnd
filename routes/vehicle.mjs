0a69844f064d43599325c430779d110815a06704
busybiee1 <busybiee1@gmail.com>
2025-02-20 20:51:34 -0600

    something wrong. cant see to know the proble, taking a break

--- routes/vehicle.mjs
+++ routes/vehicle.mjs
@@ -1,17 +1,7 @@
 import express from 'express';
-const router = express.Router();
 import VehicleCrtl from '../controllers/vehicleControllers.mjs';
-
-//Read
 router.get('/', VehicleCrtl.ReadVehicle);
-
-//Create
 router.post('/', VehicleCrtl.CreateVehicle);
-
-//Update / Put 
 router.put('/:id', VehicleCrtl.UpdateVehicle);
-
-//Delete
 router.delete('/:id', VehicleCrtl.DeleteVehicle);
-
 export default router;
