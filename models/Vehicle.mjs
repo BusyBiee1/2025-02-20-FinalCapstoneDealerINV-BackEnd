434b8eeec3cf6b7d39391200797db2d309234976
busybiee1 <busybiee1@gmail.com>
2025-02-22 22:44:37 -0600

    added index to schema tested

--- models/Vehicle.mjs
+++ models/Vehicle.mjs
@@ -1,17 +1,13 @@
-import express from 'express';
-import { searchVehicles, getVehicles, addVehicle, updateVehicle, deleteVehicle } from '../controllers/vehicleController.mjs';
+import mongoose from 'mongoose';
 
-const router = express.Router();
+const vehicleSchema = new mongoose.Schema({
+  v_id: { type: Number, required: true, unique: true, index: true },
+  make: { type: String, required: true },
+  model: { type: String, required: true },
+  color: { type: String, required: true },
+  year: { type: String, required: true },
+});
 
-// Search
-router.get('/search', searchVehicles);
-//Read
-router.get('/', getVehicles);
-//Create
-router.post('/', addVehicle);
-//Update / Put 
-router.put('/:id', updateVehicle);
-//Delete
-router.delete('/:id', deleteVehicle);
+vehicleSchema.index({ v_id: 1 });
 
-export default router;
+export default mongoose.model('Vehicle', vehicleSchema);
